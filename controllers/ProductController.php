3ffa084f1fcd0d2da3794adcc70774de6cef319a
vmiron <itmironl2@gmail.com>
2018-05-16 01:00:48 +0300

    add view template

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,9 +1,12 @@
 <?php
+
+require_once ROOT . '/models/Products.php';
+
 class ProductController
 {
-    public function actionIndex()
+    public function actionList()
     {
-        echo "NewsController action inssadex";
-        return true;
+        $products = Products::getProductsList('https://www.w3schools.com/xml/simple.xml');
+        require_once ROOT . '/views/products/index.php';
     }
 }
